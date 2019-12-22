9683dbb6ad5514cfb8d3122bb0472c8336d0774f
Rozemarin <45430474+Rozemarin@users.noreply.github.com>
2019-12-22 20:39:59 +0300

    Собиралка ханойской башни с сохранением

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Console_Game
 {
@@ -7,23 +8,23 @@ namespace Console_Game
         static int mode; // кол-во колец
         static int[][] towers; // список башен
         static bool gameStatus = true; // статус игры
+        static string filepath = "saved.txt";
 
         static void Main(string[] args)
         {
             Console.Title = "Tower of Hanoi";
-            Console.WriteLine("Choose the game mode (from 1 to 9):");
-            mode = Convert.ToInt32(Console.ReadLine());
-            while (mode >= 10 || mode < 1)
+
+            using (StreamWriter sw = new StreamWriter(filepath, true))
             {
-                Console.Clear();
-                Console.WriteLine("Try again");
-                Console.WriteLine("Choose the game mode (from 1 to 9):");
-                mode = Convert.ToInt32(Console.ReadLine());
+                sw.Write("");
             }
-            create_towers();
+
+            create_file_towers();
+
+            // create_towers();
             int from, to;
             render();
-            
+
             // начало игры
             while (gameStatus)
             {
@@ -39,15 +40,116 @@ namespace Console_Game
                 else
                 {
                     render();
-                    Console.WriteLine("Please, follow the rules");
+                    Console.WriteLine("Пожалуйста, следуйте правилам");
                     continue;
                 }
                 if (check_win())
                 {
+                    Console.WriteLine("Вы сделали это!");
+                    gameStatus = false;
                     break;
                 }
+                autosave();
             }
-            Console.WriteLine("You did it!");
+        }
+
+        // сохранение
+        static void autosave()
+        {
+            using (StreamWriter sw = new StreamWriter(filepath, false))
+            {
+                sw.Write(mode);
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < mode; j++)
+                    {
+                        sw.Write(towers[i][j]);
+                    }
+                }
+            }
+        }
+
+        // создание башен
+        static void create_file_towers()
+        {
+            if (!(last_game()))
+            {
+                using (StreamWriter sw = new StreamWriter(filepath, false))
+                {
+                    sw.Write("");
+                }
+                using (StreamWriter sw = new StreamWriter(filepath, true))
+                {
+                    choose_mode();
+                    sw.Write(mode);
+                    for (int i = 0; i < 3; i++)
+                    {
+                        for (int j = 0; j < mode; j++)
+                        {
+                            if (i == 0)
+                            {
+                                sw.Write(j + 1);
+                            }
+                            else
+                            {
+                                sw.Write(0);
+                            }
+                        }
+                    }
+                }
+            }
+            using (StreamReader sr = new StreamReader(filepath))
+            {
+                // перезапись башен из файла в список
+                string temp = Convert.ToString((char)sr.Read());
+                int temp1 = Int32.Parse(temp);
+                mode = temp1;
+                towers = new int[3][];
+                for (int i = 0; i < 3; i++)
+                {
+                    towers[i] = new int[mode];
+                    for (int j = 0; j < mode; j++)
+                    {
+                        temp = Convert.ToString((char)sr.Read());
+                        temp1 = Int32.Parse(temp);
+                        towers[i][j] = temp1;
+                    }
+                }
+            }
+        }
+
+        // продолжить игру?
+        static bool last_game()
+        {
+            using (StreamReader sr = new StreamReader(filepath))
+            {
+                if (sr.Peek() != -1)
+                {
+                    Console.WriteLine("Продолжить игру?");
+                    Console.WriteLine("Да: 1 Нет: 2");
+                    int answer = Convert.ToInt32(Console.ReadLine());
+                    if (answer == 1)
+                    {
+                        Console.Clear();
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+
+        // выбор режима
+        static void choose_mode()
+        {
+            Console.WriteLine("Выберите количество колец (от 1 до 9):");
+            mode = Convert.ToInt32(Console.ReadLine());
+            while (mode >= 10 || mode < 1)
+            {
+                Console.Clear();
+                Console.WriteLine("Попробуйте ещё раз");
+                Console.WriteLine("Выберите количество колец (от 1 до 9):");
+                mode = Convert.ToInt32(Console.ReadLine());
+            }
         }
 
         // вывод башен
@@ -64,7 +166,7 @@ namespace Console_Game
                 {
                     temp_towers[i][j] = towers[i][j];
                 }
-                if (temp_towers[i][mode - 1] == 0 && temp_towers [i][0] != 0)
+                if (temp_towers[i][mode - 1] == 0 && temp_towers[i][0] != 0)
                 {
                     int cnt = 0;
                     while (temp_towers[i][mode - 1] == 0)
@@ -137,7 +239,7 @@ namespace Console_Game
                 Console.WriteLine();
             }
         }
-        
+
         // проверка конца игры
         static bool check_win()
         {
@@ -145,13 +247,16 @@ namespace Console_Game
             {
                 if (towers[i][mode - 1] != 0)
                 {
+                    using (StreamWriter sw = new StreamWriter(filepath, false))
+                    {
+                        sw.Write("");
+                    }
                     return true;
                 }
             }
             return false;
-
         }
-        
+
         // перестановка колец
         static void put(int from, int to)
         {
@@ -166,11 +271,11 @@ namespace Console_Game
             }
             towers[from][mode - 1] = 0;
         }
-        
+
         // проверка соблюдений правил
         static bool can_put(int from, int to)
         {
-            if (from > mode || to > mode)
+            if (from > 3 || to > 3)
             {
                 return false;
             }
@@ -189,27 +294,6 @@ namespace Console_Game
             else
             {
                 return true;
-            }
-        }
-
-        // создание башен
-        static void create_towers()
-        {
-            towers = new int[3][];
-            for (int i = 0; i < 3; i++)
-            {
-                towers[i] = new int[mode];
-                for (int j = 0; j < mode; j++)
-                    {
-                    if (i == 0)
-                    {
-                        towers[i][j] = j + 1;
-                    }
-                    else
-                    {
-                        towers[i][j] = 0;
-                    }
-                }
             }
         }
     }
